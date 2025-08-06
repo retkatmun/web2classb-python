b9fa4dcfc3c7ef70817ca7f3a3660faaed1a2a6c
Retkatmun <retkatmun1@gmail.com>
2025-08-06 12:42:42 +0000

    added checker for name

--- facebook.py
+++ facebook.py
@@ -1,9 +1,19 @@
 '''
-enter_password = int(input("Enter password"))
-comfirm_password = int(input("comfirm password"))
+enter_password = int(input("Enter password:  "))
+comfirm_password = int(input("comfirm password:  "))
 
 if enter_password == comfirm_password:
     print("successful")
 else:
     print("wrong password") 
 '''
+
+user_data = ["core","tk","mp"]
+
+
+enter_data = input("Enter name:  ")
+
+if enter_data in user_data:
+    print("exist")
+else:
+    print("does not exist")
